db1b48f1edd743064519c31f42e0438862c7430f
Eero <eeero93@gmail.com>
2022-04-10 14:46:58 +0300

    add calculator buttons to js

---
+++ main.js
@@ -0,0 +1,65 @@
+let mainContainer = document.querySelector('#container');
+
+const oneBtn = document.createElement('button');
+oneBtn.textContent = "1";
+mainContainer.appendChild(oneBtn);
+
+const twoBtn = document.createElement('button');
+twoBtn.textContent = "2";
+mainContainer.appendChild(twoBtn);
+
+const threeBtn = document.createElement('button');
+threeBtn.textContent = "3";
+mainContainer.appendChild(threeBtn);
+
+const fourBtn = document.createElement('button');
+fourBtn.textContent = "4";
+mainContainer.appendChild(fourBtn);
+
+const fiveBtn = document.createElement('button');
+fiveBtn.textContent = "5";
+mainContainer.appendChild(fiveBtn );
+
+const sixBtn = document.createElement('button');
+sixBtn.textContent = "6";
+mainContainer.appendChild(sixBtn);
+
+const sevenBtn = document.createElement('button');
+sevenBtn.textContent = "7";
+mainContainer.appendChild(sevenBtn);
+
+const eightBtn = document.createElement('button');
+eightBtn.textContent = "8";
+mainContainer.appendChild(eightBtn);
+
+const nineBtn = document.createElement('button');
+nineBtn.textContent = "9";
+mainContainer.appendChild(nineBtn);
+
+const zeroBtn = document.createElement('button');
+zeroBtn.textContent = "0";
+mainContainer.appendChild(zeroBtn);
+
+const addBtn = document.createElement('button');
+addBtn .textContent = "+";
+mainContainer.appendChild(addBtn );
+
+const substractBtn = document.createElement('button');
+substractBtn.textContent = "-";
+mainContainer.appendChild(substractBtn);
+
+const diviveBtn = document.createElement('button');
+diviveBtn.textContent = "/";
+mainContainer.appendChild(diviveBtn);
+
+const multiplyBtn = document.createElement('button');
+multiplyBtn.textContent = "*";
+mainContainer.appendChild(multiplyBtn);
+
+const equalsBtn = document.createElement('button');
+equalsBtn .textContent = "=";
+mainContainer.appendChild(equalsBtn );
+
+const clearBtn = document.createElement('button');
+clearBtn .textContent = "clear";
+mainContainer.appendChild(clearBtn );
