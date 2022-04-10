92dd39a1bcbc44f001cd9d56f0cb4a2fe33f1878
Eero <eeero93@gmail.com>
2022-04-10 17:57:22 +0300

    add para for the inputs

--- main.js
+++ main.js
@@ -1,68 +1,78 @@
-let mainContainer = document.querySelector('#operators');
+let mainContainer = document.querySelector('#container');
+let calcContainer = document.querySelector('#operators');
+
+const calcPara = document.createElement('p');
+calcPara.textContent = '9659';
+calcContainer.appendChild(calcPara);
+
+// buttons start 
 
 const oneBtn = document.createElement('button');
 oneBtn.textContent = "1";
-mainContainer.appendChild(oneBtn);
+calcContainer.appendChild(oneBtn);
 
 const twoBtn = document.createElement('button');
 twoBtn.textContent = "2";
-mainContainer.appendChild(twoBtn);
+calcContainer.appendChild(twoBtn);
 
 const threeBtn = document.createElement('button');
 threeBtn.textContent = "3";
-mainContainer.appendChild(threeBtn);
+calcContainer.appendChild(threeBtn);
 
 const fourBtn = document.createElement('button');
 fourBtn.textContent = "4";
-mainContainer.appendChild(fourBtn);
+calcContainer.appendChild(fourBtn);
 
 const fiveBtn = document.createElement('button');
 fiveBtn.textContent = "5";
-mainContainer.appendChild(fiveBtn );
+calcContainer.appendChild(fiveBtn );
 
 const sixBtn = document.createElement('button');
 sixBtn.textContent = "6";
-mainContainer.appendChild(sixBtn);
+calcContainer.appendChild(sixBtn);
 
 const sevenBtn = document.createElement('button');
 sevenBtn.textContent = "7";
-mainContainer.appendChild(sevenBtn);
+calcContainer.appendChild(sevenBtn);
 
 const eightBtn = document.createElement('button');
 eightBtn.textContent = "8";
-mainContainer.appendChild(eightBtn);
+calcContainer.appendChild(eightBtn);
 
 const nineBtn = document.createElement('button');
 nineBtn.textContent = "9";
-mainContainer.appendChild(nineBtn);
+calcContainer.appendChild(nineBtn);
 
 const zeroBtn = document.createElement('button');
 zeroBtn.textContent = "0";
-mainContainer.appendChild(zeroBtn);
+calcContainer.appendChild(zeroBtn);
 
 const br = document.createElement('br');
-mainContainer.appendChild(br);
+calcContainer.appendChild(br);
 
 const addBtn = document.createElement('button');
 addBtn .textContent = "+";
-mainContainer.appendChild(addBtn );
+calcContainer.appendChild(addBtn );
 
 const substractBtn = document.createElement('button');
 substractBtn.textContent = "-";
-mainContainer.appendChild(substractBtn);
+calcContainer.appendChild(substractBtn);
 
 const diviveBtn = document.createElement('button');
 diviveBtn.textContent = "/";
-mainContainer.appendChild(diviveBtn);
+calcContainer.appendChild(diviveBtn);
 
 const multiplyBtn = document.createElement('button');
 multiplyBtn.textContent = "*";
-mainContainer.appendChild(multiplyBtn);
+calcContainer.appendChild(multiplyBtn);
 
 const equalsBtn = document.createElement('button');
-equalsBtn .textContent = "=";
-mainContainer.appendChild(equalsBtn );
+equalsBtn.textContent = "=";
+calcContainer.appendChild(equalsBtn);
 
 const clearBtn = document.createElement('button');
-clearBtn .textContent = "clear";
-mainContainer.appendChild(clearBtn );
+clearBtn.textContent = "clear";
+calcContainer.appendChild(clearBtn);
+
+//buttons end
+
